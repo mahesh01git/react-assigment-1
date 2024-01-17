5bc1a213658fbed2505f097d5d581dcbb41fa352
mahesh01git <151907025+mahesh01git@users.noreply.github.com>
2024-01-17 18:42:55 +0530

    Update index.js

--- src/Components/SearchComponent/index.js
+++ src/Components/SearchComponent/index.js
@@ -4,9 +4,99 @@ import './index.css'
 
 import Item from '../ItemComponent'
 
+const initialHistoryList = [
+  {
+    id: 0,
+    timeAccessed: '07:45 PM',
+    logoUrl: 'https://assets.ccbp.in/frontend/react-js/instagram-img.png',
+    title: 'Instagram',
+    domainUrl: 'instagram.com',
+  },
+  {
+    id: 1,
+    timeAccessed: '05:45 PM',
+    logoUrl: 'https://assets.ccbp.in/frontend/react-js/twitter-img.png',
+    title: 'Twitter. It’s what’s happening / Twitter',
+    domainUrl: 'twitter.com',
+  },
+  {
+    id: 2,
+    timeAccessed: '04:35 PM',
+    logoUrl: 'https://assets.ccbp.in/frontend/react-js/facebook-img.png',
+    title: 'Facebook – log in or sign up',
+    domainUrl: 'facebook.com',
+  },
+  {
+    id: 3,
+    timeAccessed: '04:25 PM',
+    logoUrl: 'https://assets.ccbp.in/frontend/react-js/linkedin-img.png',
+    title: 'LinkedIn: Log In or Sign Up',
+    domainUrl: 'linkedin.com',
+  },
+  {
+    id: 4,
+    timeAccessed: '04:00 PM',
+    logoUrl: 'https://assets.ccbp.in/frontend/react-js/hashnode-img.png',
+    title: 'Hashnode: Everything you need to start blogging as a developer!',
+    domainUrl: 'hashnode.com',
+  },
+  {
+    id: 5,
+    timeAccessed: '03:25 PM',
+    logoUrl: 'https://assets.ccbp.in/frontend/react-js/github-img.png',
+    title: 'GitHub: Where the world builds software · GitHub',
+    domainUrl: 'github.com',
+  },
+
+  {
+    id: 6,
+    timeAccessed: '02:45 PM',
+    logoUrl: 'https://assets.ccbp.in/frontend/react-js/react-img.png',
+    title: 'React – A JavaScript library for building user interfaces',
+    domainUrl: 'reactjs.org',
+  },
+  {
+    id: 7,
+    timeAccessed: '01:25 PM',
+    logoUrl: 'https://assets.ccbp.in/frontend/react-js/stackoverflow-img.png',
+    title: 'Stack Overflow - Where Developers Learn, Share, & Build Careers',
+    domainUrl: 'stackoverflow.com',
+  },
+
+  {
+    id: 8,
+    timeAccessed: '09:25 AM',
+    logoUrl: 'https://assets.ccbp.in/frontend/react-js/gmail-img.png',
+    title: 'Gmail',
+    domainUrl: 'mail.google.com',
+  },
+  {
+    id: 9,
+    timeAccessed: '09:00 AM',
+    logoUrl: 'https://assets.ccbp.in/frontend/react-js/google-img.png',
+    title: 'Google',
+    domainUrl: 'google.com',
+  },
+]
+
 class SearchItem extends Component {
+  state = {searchInput: '', updatedList: initialHistoryList}
+
+  onChangeInput = event => {
+    this.setState({searchInput: event.target.value})
+  }
+
+  onDelete = id => {
+    const {updatedList} = this.state
+    const updatedResult = updatedList.filter(each => each.id !== id)
+    this.setState({updatedList: updatedResult})
+  }
+
   render() {
-    const {initialHistoryList} = this.props
+    const {searchInput, updatedList} = this.state
+    const searchResult = updatedList.filter(each =>
+      each.title.toLowerCase().includes(searchInput.toLowerCase()),
+    )
     return (
       <div className="app-con">
         <div className="search-con">
@@ -21,15 +111,29 @@ class SearchItem extends Component {
               src="https://assets.ccbp.in/frontend/react-js/search-img.png"
               alt="search"
             />
-            <input type="search" placeholder="Search History" />
+            <input
+              className="input"
+              type="search"
+              placeholder="Search History"
+              onChange={this.onChangeInput}
+              value={searchInput}
+            />
           </div>
         </div>
         <div className="buttom-con">
-          <ul className="list-con">
-            {initialHistoryList.map(eachItem => (
-              <Item key={eachItem.id} itemDetails={eachItem} />
-            ))}
-          </ul>
+          {updatedList.length < 1 ? (
+            <p>There is no result</p>
+          ) : (
+            <ul className="list-con">
+              {searchResult.map(eachItem => (
+                <Item
+                  key={eachItem.id}
+                  itemDetails={eachItem}
+                  deleteHistory={this.onDelete}
+                />
+              ))}
+            </ul>
+          )}
         </div>
       </div>
     )
@@ -37,3 +141,4 @@ class SearchItem extends Component {
 }
 
 export default SearchItem
+
