5009a70fbc33dfa3712a38ff50555b30f9dc69ed
mahesh01git <151907025+mahesh01git@users.noreply.github.com>
2024-01-17 18:43:26 +0530

    Update index.js

--- src/Components/ItemComponent/index.js
+++ src/Components/ItemComponent/index.js
@@ -1,17 +1,27 @@
 import './index.css'
 
 const Item = props => {
-  const {itemDetails} = props
+  const {itemDetails, deleteHistory} = props
 
-  const {timeAccessed, logoUrl, title, domainUrl} = itemDetails
+  const {timeAccessed, logoUrl, title, domainUrl, id} = itemDetails
+
+  const deleteItem = () => {
+    deleteHistory(id)
+  }
 
   return (
     <li className="item-con">
       <p>{timeAccessed}</p>
-      <img className="logo" src={logoUrl} alt={title} />
-      <p>
-        {title} <span>{domainUrl}</span>
-      </p>
+      <img className="logo" src={logoUrl} alt="domain logo" />
+      <p>{title}</p>
+      <p>{domainUrl}</p>
+      <button onClick={deleteItem} type="button" data-testid="delete">
+        <img
+          src="https://assets.ccbp.in/frontend/react-js/delete-img.png"
+          alt="delete"
+          className="delete-icon"
+        />
+      </button>
     </li>
   )
 }
